d34e9026546fe906e602332201b2699d4d4d1074
Margarete <margarete.dippel@student.hpi.de>
2021-11-12 16:01:49 +0100

    author page displays author details (closes #6)

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -5,11 +5,11 @@ class AuthorsController < ApplicationController
     @author = Author.new(author_params)
 
     @author.save
-    redirect_to root_path, notice: 'Success!' #@author
+    redirect_to @author
   end
-  #def show
-  #  @author = Author.find(params[:id])
-  #end
+  def show
+    @author = Author.find(params[:id])
+  end
 
   private
     def author_params
