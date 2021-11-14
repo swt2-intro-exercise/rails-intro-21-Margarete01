86c1c9a2d3274e3fdec339ba8964decc50547c8f
Margarete <margarete.dippel@student.hpi.de>
2021-11-14 16:45:47 +0100

    paper doesn't validate without title (closes #13)

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -10,4 +10,32 @@ describe "a paper", type: :model do
     expect(paper.venue).to eq("Mind 49: 433-460")
     expect(paper.year).to eq(1950)
   end
+  it "shouldn't validate without title" do
+    paper = Paper.new
+    paper.title = nil
+    paper.venue = "Mind 49: 433-460"
+    paper.year = 1950
+    expect(paper).to_not be_valid
+  end
+  it "shouldn't validate without venue" do
+    paper = Paper.new
+    paper.title = "COMPUTING MACHINERY AND INTELLIGENCE"
+    paper.venue = nil
+    paper.year = 1950
+    expect(paper).to_not be_valid
+  end
+  it "shouldn't vaildate without year" do
+    paper = Paper.new()
+    paper.title = "COMPUTING MACHINERY AND INTELLIGENCE"
+    paper.venue = "Mind 49: 433-460"
+    paper.year = nil
+    expect(paper).to_not be_valid
+  end
+  it "shouldn't vaildate without year having a numerical value" do
+    paper = Paper.new()
+    paper.title = "COMPUTING MACHINERY AND INTELLIGENCE"
+    paper.venue = "Mind 49: 433-460"
+    paper.year = "nineteen hundred and fifty"
+    expect(paper).to_not be_valid
+  end
 end
